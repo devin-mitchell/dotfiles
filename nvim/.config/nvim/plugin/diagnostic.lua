ee156ac5cddab15569c4ca0ae185aabe29af1f50
Devin Mitchell <devin.a.mitchell@gmail.com>
2022-08-27 13:00:48 -0600

    added diagnostic navigation keybindings

--- nvim/.config/nvim/plugin/diagnostic.lua
+++ nvim/.config/nvim/plugin/diagnostic.lua
@@ -1,3 +1,5 @@
+local map = vim.keymap.set
+
 vim.diagnostic.config {
   underline = false,
   virtual_text = {
@@ -14,3 +16,7 @@ for type, icon in pairs(signs) do
   local highlight = "DiagnosticSign" .. type
   vim.fn.sign_define(highlight, { text = icon, texthl = highlight })
 end
+
+map('n', '[d', vim.diagnostic.goto_prev)
+map('n', ']d', vim.diagnostic.goto_next)
+map('n', '<C-g>', vim.diagnostic.open_float)
