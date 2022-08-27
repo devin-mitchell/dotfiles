e22f22a7c674c167853b62791c7542f4c7991194
Devin Mitchell <devin.a.mitchell@gmail.com>
2022-08-27 12:54:57 -0600

    add autoComplete to nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -69,6 +69,19 @@ vim.cmd([[packadd packer.nvim]])
     end
   }
 
+  use {
+    'hrsh7th/nvim-cmp',
+    config = function() require('autoComplete') end,
+    requires = {
+      'L3MON4D3/LuaSnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-nvim-lua'
+    }
+  }
+
 	-- THEMES
 	use({
 		"catppuccin/nvim",
