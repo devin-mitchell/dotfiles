129f44ee7f98796c9263ea96c844569809b0eea6
Devin Mitchell <devin.a.mitchell@gmail.com>
2022-12-14 14:45:10 -0700

    Change nvim and kitty themes

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -4,16 +4,16 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd([[packadd packer.nvim]])
 
-	return require("packer").startup(function()
-  	-- Packer can manage itself
-  	use("wbthomason/packer.nvim")
-  
-  use {
-    'vim-test/vim-test',
-    config = function()
-      require("vimTest")
-    end
-  }
+return require("packer").startup(function()
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
+
+	use({
+		"vim-test/vim-test",
+		config = function()
+			require("vimTest")
+		end,
+	})
 
 	use({
 		"nvim-telescope/telescope.nvim",
@@ -48,136 +48,154 @@ vim.cmd([[packadd packer.nvim]])
 		end,
 	})
 
-  use {
-    'lewis6991/gitsigns.nvim',
-    config = function() require('git') end, 
-    -- tag = 'release' -- To use the latest release
-  }
-
-  use({
-    'NTBBloodbath/galaxyline.nvim',
-    branch = 'main',
-    config = function()
-      require("galaxyline.themes.eviline")
-    end,
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-  })
-
 	use({
-		'elixir-editors/vim-elixir'
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("git")
+		end,
+		-- tag = 'release' -- To use the latest release
 	})
 
 	use({
-		'mhinz/vim-mix-format'
+		"NTBBloodbath/galaxyline.nvim",
+		branch = "main",
+		config = function()
+			require("galaxyline.themes.eviline")
+		end,
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
 	})
 
 	use({
-		'numToStr/FTerm.nvim'
+		"elixir-editors/vim-elixir",
 	})
 
-   use {
-    'norcalli/nvim-colorizer.lua',
-    config = function()
-      require('colorizer').setup()
-    end
-  }
+	use({
+		"mhinz/vim-mix-format",
+	})
 
-  use {
-    "folke/which-key.nvim",
-    config = function()
-      require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  }
+	use({
+		"numToStr/FTerm.nvim",
+	})
 
-  use {
-    'tpope/vim-fugitive',
-    requires = {'tpope/vim-rhubarb'}
-  }
+	use({
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	})
 
-  use {
-    'hrsh7th/nvim-cmp',
-    config = function() require('autoComplete') end,
-    requires = {
-      'L3MON4D3/LuaSnip',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'hrsh7th/cmp-nvim-lua'
-    }
-  }
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
 
-  use 'tpope/vim-commentary'
+	use({
+		"tpope/vim-fugitive",
+		requires = { "tpope/vim-rhubarb" },
+	})
 
-  use 'tpope/vim-endwise'
+	use({
+		"hrsh7th/nvim-cmp",
+		config = function()
+			require("autoComplete")
+		end,
+		requires = {
+			"L3MON4D3/LuaSnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-nvim-lua",
+		},
+	})
 
-  use 'vyperlang/vim-vyper' 
+	use("tpope/vim-commentary")
 
-  use { 
-    'glacambre/firenvim', 
-    run = function() vim.fn['firenvim#install'](0) end
-    }
+	use("tpope/vim-endwise")
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    requires = {
-	      'nvim-treesitter/nvim-treesitter-textobjects',
-	      'nvim-treesitter/playground'
-	    },
-	    config = function() require("tree-sitter") end
-	  }
+	use("vyperlang/vim-vyper")
+
+	use({
+		"glacambre/firenvim",
+		run = function()
+			vim.fn["firenvim#install"](0)
+		end,
+	})
+
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		requires = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"nvim-treesitter/playground",
+		},
+		config = function()
+			require("tree-sitter")
+		end,
+	})
 
 	-- THEMES
-  use({
-    "rafamadriz/neon",
-    config = function()
-      vim.g.neon_style = "doom"
-      vim.cmd([[colorscheme neon]])
-    end,
-  })
+	use({
+		"rafamadriz/neon",
+		config = function()
+			vim.g.neon_style = "doom"
+			-- vim.cmd([[colorscheme neon]])
+		end,
+	})
 
-  use({
-    'Yazeed1s/minimal.nvim',
-    config = function()
-      -- vim.cmd([[colorscheme minimal]])
-    end,
-  })
+	use({
+		"Yazeed1s/minimal.nvim",
+		config = function()
+			-- vim.cmd([[colorscheme minimal]])
+		end,
+	})
 
-  use({
-    'rockerBOO/boo-colorscheme-nvim',
-    config = function()
-     -- vim.cmd([[colorscheme boo]])
-    end,
-  })
+	use({
+		"navarasu/onedark.nvim",
+		config = function()
+			require("onedark").setup({
+				style = "warmer",
+			})
+			vim.cmd([[colorscheme onedark]])
+		end,
+	})
 
-   use({
-     "catppuccin/nvim",
-     as = "catppuccin",
-     config = function()
-       vim.g.catppuccin_flavour = "mocha"
---       vim.cmd([[colorscheme catppuccin]])
-     end,
-   })
+	use({
+		"rockerBOO/boo-colorscheme-nvim",
+		config = function()
+			-- vim.cmd([[colorscheme boo]])
+		end,
+	})
 
-   use({
-     "folke/tokyonight.nvim",
-     config = function()
-       require 'tokyonight'.setup({
-         style = 'night'
-       })
-    --   vim.cmd[[colorscheme tokyonight]]
-     end,
-   })
+	use({
+		"catppuccin/nvim",
+		as = "catppuccin",
+		config = function()
+			vim.g.catppuccin_flavour = "mocha"
+			--       vim.cmd([[colorscheme catppuccin]])
+		end,
+	})
 
-	 use({
-     "challenger-deep-theme/vim",
-     as = "challenger_deep",
-     config = function()
-      -- vim.cmd[[colorscheme challenger_deep]]
-     end,
-   })
+	use({
+		"folke/tokyonight.nvim",
+		config = function()
+			require("tokyonight").setup({
+				style = "night",
+			})
+			--   vim.cmd[[colorscheme tokyonight]]
+		end,
+	})
+
+	use({
+		"challenger-deep-theme/vim",
+		as = "challenger_deep",
+		config = function()
+			-- vim.cmd[[colorscheme challenger_deep]]
+		end,
+	})
 end)
